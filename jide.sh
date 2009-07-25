caa8f96945b7927b197df7c39fd7fd6610750fb5
Luigi Capraro <luigi.capraro@gmail.com>
2009-07-25 07:49:46 +0200

    JIDE (Java IDE) Project start

---
+++ jide.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+###############################################################################
+#                        JIDE: The Java IDE Script                            #
+###############################################################################
+# Author    : Luigi Capraro                                                   #
+# E-mail    : luigi.capraro@gmail.com                                         #
+# Copyright : GPL v.3                                                         #
+#                                                                             #
+# jide.sh                                                                     #
+# This file is part of JIDE                                                   #
+#                                                                             #
+# Copyright (C) 2009 - Luigi Capraro                                          #
+#                                                                             #
+# JIDE is free software; you can redistribute it and/or modify                #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation; either version 2 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# JIDE is distributed in the hope that it will be useful,                     #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with JIDE; if not, write to the Free Software                         #
+# Foundation, Inc., 51 Franklin St, Fifth Floor,                              #
+# Boston, MA  02110-1301  USA                                                 #
+#                                                                             #
+###############################################################################
+ 
+ 
+ 
+### COMMON FUNCTION      ###
+
+### END COMMON FUNCTION  ###
+
+
+### HELP SECTION         ###
+
+jide_help_config() {
+
+}
+
+jide_help_init() {
+
+}
+
+jide_help_compile() {
+
+}
+
+jide_help_run() {
+
+}
+
+jide_help_clean() {
+
+}
+
+
+jide_help_delete() {
+
+}
+
+jide_help_archive() {
+
+}
+
+jide_help() {
+
+}
+
+### END HELP SECTION     ###
+
+### COMMANDS SECTION     ###
+jide_config() {
+
+}
+
+jide_init() {
+
+}
+
+jide_compile() {
+
+}
+
+jide_run() {
+
+}
+
+jide_clean() {
+
+}
+
+
+jide_delete() {
+
+}
+
+jide_archive() {
+
+}
+
+
+jide_main() {
+
+	exit $?
+}
+### END COMMANDS SECTION ###
+
+
+### MAIN PROGRAM         ###
+jide_main $*
+
+
