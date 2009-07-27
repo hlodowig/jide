8f7318a0b5081b52a36c9d6f5516778b6996cb39
Luigi Capraro <luigi.capraro@gmail.com>
2009-07-27 04:51:26 +0200

    JIDE: Refactoring sul comando run

--- lib/jide-config.sh
+++ lib/jide-config.sh
@@ -29,17 +29,17 @@ jide_help_config()
 
 jide_config() 
 {
-	echo "JIDE CONFIGURATION"
+#	echo "JIDE CONFIGURATION"
 	
-	JIDE_SCRIPT=$(get_absolute_path $JIDE_PROGNAME)
-	JIDE_HOME=$(dirname $JIDE_SCRIPT)
+#	JIDE_SCRIPT=$(get_absolute_path $JIDE_PROGNAME)
+
 	
-	echo "JIDE_SCRIPT=$JIDE_SCRIPT"
-	echo "JIDE_HOME=$JIDE_HOME"
+#	echo "JIDE_SCRIPT=$JIDE_SCRIPT"
+#	echo "JIDE_HOME=$JIDE_HOME"
 	
 	if [ -n "$1" ]; then	
 		if [ -f "$1" ]; then
-			echo "Configuration file: $1"	
+			#echo "Configuration file: $1"	
 			source $1
 		else
 			print_error "Configuration file: $1 not found"; exit 2;
@@ -51,14 +51,12 @@ jide_config()
 		             "/usr/local/etc/jide/$JIDE_CONFIGFILE"
 		do
 			if [ -f "$cfile" ]; then
-				echo "Configuration file: $cfile"	
+				#echo "Configuration file: $cfile"	
 				source $cfile
 				break	
 			fi
 		done
 	fi
-	
-	echo
 	
 	cd $JIDE_PROJECT_HOME
 
